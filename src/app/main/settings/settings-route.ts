25d441dff1bb4d08b933734b0304876e5d61b3a3
fuyb0329 <fuyb0329@thundersoft.com>
2018-03-02 19:58:11 +0800

    打包

--- src/app/main/settings/settings-route.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-import {SettingsComponent} from './settings.component';
-
-export const settingsRoutes: Routes = [
-  {
-    path: '',
-    component: SettingsComponent,
-  },
-];
